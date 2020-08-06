be201b739836363cf303e75904defd25406de69f
JorgeAmadoFlorentino <qualitechdevops@gmail.com>
2020-08-05 23:15:19 -0300

    Commit 9

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -86,3 +87,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'login'
